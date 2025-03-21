f55f9e1dd2148f7447d47dc5076a772efe53e51e
0x676e67 <gngppz@gmail.com>
2025-03-21 19:36:03 +0800

    build: update workflows action (#61)

--- boring/src/ssl/test/extensions.rs
+++ boring/src/ssl/test/extensions.rs
@@ -2,7 +2,10 @@ use crate::ssl::ExtensionType;
 
 #[test]
 fn test_exntension_order_index() {
-    for (i, ext) in ExtensionType::BORING_SSLEXTENSION_PERMUTATION.iter().enumerate() {
+    for (i, ext) in ExtensionType::BORING_SSLEXTENSION_PERMUTATION
+        .iter()
+        .enumerate()
+    {
         assert_eq!(ExtensionType::index_of(*ext), Some(i));
     }
 }
