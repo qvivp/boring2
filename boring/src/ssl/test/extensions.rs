2576807382de9f53758ea8999406cb93aa03d755
0x676e67 <gngppz@gmail.com>
2025-03-21 18:28:13 +0800

    chore: clippy fix

--- boring/src/ssl/test/extensions.rs
+++ boring/src/ssl/test/extensions.rs
@@ -2,9 +2,7 @@ use crate::ssl::ExtensionType;
 
 #[test]
 fn test_exntension_order_index() {
-    let mut i = 0;
-    for ext in ExtensionType::BORING_SSLEXTENSION_PERMUTATION {
+    for (i, ext) in ExtensionType::BORING_SSLEXTENSION_PERMUTATION.iter().enumerate() {
         assert_eq!(ExtensionType::index_of(*ext), Some(i));
-        i += 1;
     }
 }
