58d2b58d70bb86a61f5a6e03f7dc5d1fdfbe91ab
0x676e67 <gngppz@gmail.com>
2025-06-18 22:19:30 +0800

    docs: update prefer chacha20 option docs (#81)

--- boring/src/ssl/mod.rs
+++ boring/src/ssl/mod.rs
@@ -1910,7 +1910,11 @@ impl SslContextBuilder {
         unsafe { ffi::SSL_CTX_set_aes_hw_override(self.as_ptr(), enable as _) }
     }
 
-    /// Sets whether the aes chacha20 preference should be enabled.
+    /// Sets whether the ChaCha20 preference should be enabled.
+    ///
+    /// Controls the priority of TLS 1.3 cipher suites. When set to `true`, the client prefers:
+    /// AES_128_GCM, CHACHA20_POLY1305, then AES_256_GCM. Useful in environments with specific
+    /// encryption requirements.
     #[cfg(not(feature = "fips"))]
     #[corresponds(SSL_CTX_set_prefer_chacha20)]
     pub fn set_prefer_chacha20(&mut self, enable: bool) {
